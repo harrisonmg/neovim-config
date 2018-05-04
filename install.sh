6d150fffc8c6bba8388e7d37ad1b56b7454df1d1
harrisonmg <gieraltowski.har@gmail.com>
2018-05-04 15:19:09 -0400

    changed aliases added to bashrc

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # make alias
-echo -e "# adios vi\nalias vi='nvim'\nalias vim='nvim'\nexport EDITOR='nvim'" >> ~/.bashrc
+echo -e "\n\n# adios vi\nalias nv='nvim'\nexport EDITOR='nvim'" >> ~/.bashrc
 
 # open neovim and install plugins
 nvim -c PlugInstall
