fb1d24534e59dcf94619f06a51f336c50b567721
Harrison MG <gieraltowski.har@gmail.com>
2020-10-30 19:52:40 -0400

    updated anaconda path

--- Profile.ps1
+++ Profile.ps1
@@ -1,3 +1,3 @@
 Set-PSReadLineOption -EditMode Emacs
-$env:Path+= ";C:\Users\Harrison\anaconda3\Scripts"
+$env:Path+= ";C:\Users\Harrison\anaconda3\Scripts;C:\Users\Harrison\anaconda3\Library\bin"
 Set-Alias -name p3 -value C:\Users\Harrison\anaconda3\python.exe
