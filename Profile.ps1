6fabbbd0b5654f873d3f555244b65214a14bd33d
Harrison MG <gieraltowski.har@gmail.com>
2020-10-30 19:56:37 -0400

    disable powershell bell

--- Profile.ps1
+++ Profile.ps1
@@ -1,3 +1,4 @@
 Set-PSReadLineOption -EditMode Emacs
+Set-PSReadlineOption -BellStyle None
 $env:Path+= ";C:\Users\Harrison\anaconda3\Scripts;C:\Users\Harrison\anaconda3\Library\bin"
 Set-Alias -name p3 -value C:\Users\Harrison\anaconda3\python.exe
