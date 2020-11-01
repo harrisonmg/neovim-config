5bcb299de867de46c2efe89dafbcec9a3bac035c
Harrison MG <gieraltowski.har@gmail.com>
2020-10-31 23:50:51 -0400

    updated pack and unpack to include settings.json

--- pack.sh
+++ pack.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cd "${0%/*}"
 cp ~/.config/nvim/init.vim .
+cp /mnt/c/Users/Harrison/AppData/Local/Packages/Microsoft.WindowsTerminal_*/LocalState/settings.json .
--- unpack.sh
+++ unpack.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cd "${0%/*}"
 cp init.vim ~/.config/nvim/
+cp settings.json /mnt/c/Users/Harrison/AppData/Local/Packages/Microsoft.WindowsTerminal_*/LocalState/
